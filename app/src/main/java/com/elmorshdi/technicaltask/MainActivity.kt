c189cd163bd82d7aa37122a3bcc7b099b45cc314
elmorshdi53 <elmorshdi53@std.mans.edu.eg>
2022-08-02 23:03:27 +0200

    setUp ui

--- app/src/main/java/com/elmorshdi/technicaltask/MainActivity.kt
+++ app/src/main/java/com/elmorshdi/technicaltask/MainActivity.kt
@@ -2,10 +2,37 @@ package com.elmorshdi.technicaltask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+    private var doubleBackToExitPressedOnce: Boolean = false
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_Activity_nav_host_fragment) as NavHostFragment
+        navHostFragment.navController
+
+        doubleBackToExitPressedOnce = false
     }
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, " Please click BACK again to exit ", Toast.LENGTH_SHORT).show()
+
+        Handler(Looper.getMainLooper()).postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+
+
+    }
+
+
 }
